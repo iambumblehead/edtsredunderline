b875648bcd3e8c5999e4395930b95df1a18dc532
chris <chris@bumblehead.com>
2015-01-06 18:00:30 -0800

    added start and stop functions to uuid test

--- test/uuid_test.erl
+++ test/uuid_test.erl
@@ -4,32 +4,27 @@
 -include_lib("eunit/include/eunit.hrl").
 
 start() -> 
-    io:format(user, "~n~n seed quickrand ~p~n~n", [edtsredunderline:start()]).
+    %% start is called, quickrand:seed() returns 'ok', but uuidtest fails
+    io:format(user, "~n~n seed quickrand ~p~n~n", [quickrand:seed()]).
 
 stop(_PID) ->
     edtsredunderline:stop().
 
 uuid_isnotrandom() ->
+    %%quickrand:seed(), %% <- uncomment this, test passes
     Result = edtsredunderline:uuidtest(),
     SameResult = "01056c26-74c1-49f9-9c40-5162c8c7c1aa",
     io:format(user, "~n~n info ~p~n~n", [{SameResult, edtsredunderline:uuidtest(), edtsredunderline:uuidtest(), edtsredunderline:uuidtest()}]),
-    SameResult =:= Result.
+    SameResult =/= Result.
 
-do_api_test_() ->
-    {"Test: is uuid predictable?",
-     {setup,
-      fun start/0,
-      fun stop/1,
-      fun (_) ->
-              [
-               ?_assertMatch(true, uuid_isnotrandom())
-               ]
-      end
-     }}.
-
-
-
-
+generator_test_() ->
+{setup, fun start/0, fun stop/1,
+ fun (_) ->
+        [
+         ?_assertMatch(true, uuid_isnotrandom())
+        ]
+ end
+}.
 
 
 
